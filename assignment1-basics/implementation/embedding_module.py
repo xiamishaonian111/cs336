4e9cb3c7d2431ac1bbd6cdd2c62858a9db8d7760
alexyang <xiamishaonian111@gmail.com>
2026-01-29 12:05:27 +0000

    implement embedding module

--- assignment1-basics/implementation/embedding_module.py
+++ assignment1-basics/implementation/embedding_module.py
@@ -13,8 +13,12 @@ class Embedding(nn.Module):
 			device: torch.device | None = None Device to store the parameters on
 			dtype: torch.dtype | None = None Data type of the parameters
 		"""
-		super().
+		super().__init__()
+		self.num_embeddings = num_embeddings
+		self.embedding_dim = embedding_dim
+		self.weights = nn.Parameter(torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype))
+		nn.init.trunc_normal_(self.weights, mean=0.0, std=1.0, a=-3.0, b=3.0)
 
 	def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
 		"""Lookup the embedding vectors for the given token IDs."""
-		raise NotImplementedError
+		return self.weights[token_ids]
